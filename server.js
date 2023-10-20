9c5ca6340d6faeb9880fe5adb2ba0667e9d1ff2e
WenJenChun <daydreamsss@hotmail.com>
2023-10-20 23:13:47 +0100

    commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // 'db.json' 是您的 JSON 数据文件
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3000; // 使用环境变量或默认端口
+const port = process.env.PORT || 3000; 
 
 server.use(middlewares);
 server.use(router);
